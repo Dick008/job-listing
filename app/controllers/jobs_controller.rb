90b8feb02aca8a7d8d7540d24b85649ccec17637
陈彦锋 <chenyanfenghappy@163.com>
2017-04-16 16:40:38 +0800

    order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,14 @@
 class JobsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
-    @jobs = Job.where(:is_hidden => false)
+    @jobs = case params[:order]
+    when "by_upper_bound"
+      Job.published.order("wage_upper_bound DESC")
+    when "by_lower_bound"
+      Job.published.order("wage_lower_bound DESC")
+    else
+      Job.published.resent
+    end
   end
 
   def new
